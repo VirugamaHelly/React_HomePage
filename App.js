4cdf1c971d1390da3896d6b3f0eee45080b3192d
HellyVirugama <virugamahelly@gmail.com>
2024-09-20 11:23:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+
+
+////sesse
+import React from 'react'
+import Header from './component/SeeseHomePage/Header/Header'
+import First from './component/SeeseHomePage/Firstpart/First'
+import Second from './component/SeeseHomePage/Second/Second'
+import LastPart from './component/SeeseHomePage/LastPart/LastPart'
+import ForthPart from './component/SeeseHomePage/ForthPart/ForthPart'
+import Footer from './component/SeeseHomePage/Footer/Footer'
+
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <First/>
+      <Second/>
+      <LastPart/>
+      <ForthPart/>
+      <Footer/>
+    </div>
+  )
+}
